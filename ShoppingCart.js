e3b4e8fc26e9408709918beec31a0727a03671ae
Ben Kang <rkdgusdn9@gmail.com>
2020-11-11 22:28:01 +1300

    initial commit
    first commit

---
+++ ShoppingCart.js
@@ -0,0 +1,9 @@
+function selectS() {
+  document.getElementById("sizeselect").innerHTML = "S";
+}
+function selectM() {
+  document.getElementById("sizeselect").innerHTML = "M";
+}
+function selectL() {
+  document.getElementById("sizeselect").innerHTML = "L";
+}
